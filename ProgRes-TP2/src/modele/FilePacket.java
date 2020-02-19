58bb646169b07154072777bf5e501290e06d2dab
Andrew <1897654@cegeplimoilou.ca>
2020-02-19 14:24:46 -0500

    Class FilePacket created

---
+++ ProgRes-TP2/src/modele/FilePacket.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.io.File;
+
+/** Object qui respresent un fichier envoyé par réseau.
+ *  Ça utilise un proprieté «type» pour identifier l'objet comme un fichier quand reçu par l'application.
+ * 
+ * @author 1897654
+ */
+public class FilePacket {
+    private String type;
+    private File file;
+
+    public FilePacket(File file) {
+        this.type = "File";
+        this.file = file;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public File getFile() {
+        return file;
+    }
+}
