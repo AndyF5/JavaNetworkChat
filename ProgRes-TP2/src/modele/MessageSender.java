506b17d07c021f928cccfede06fb9cbf74b9a093
Hamdi <benslim.hamdi@gmail.com>
2020-02-19 15:28:42 -0500

    message sender updated

--- ProgRes-TP2/src/modele/MessageSender.java
+++ ProgRes-TP2/src/modele/MessageSender.java
@@ -5,10 +5,44 @@
  */
 package modele;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 1897483
  */
-public class MessageSender {
-    
+public class MessageSender implements Runnable {
+
+    private final Collection<Message> conversation;
+    private final Message message;
+    private final Socket socket;
+
+    public MessageSender(Collection<Message> conversation, Message message, Socket socket) {
+        this.conversation = conversation;
+        this.message = message;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized(socket){
+                OutputStream out = socket.getOutputStream();
+                ObjectOutputStream outMessage = new ObjectOutputStream(out);
+                outMessage.writeObject(message);
+                outMessage.flush();
+                socket.shutdownOutput();
+            }
+           
+        } catch (IOException ex) {
+            Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
